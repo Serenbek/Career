40a95a73da8821df308746c836edd680031268a0
daniihehyhe <115088264+daniihehyhe@users.noreply.github.com>
2023-04-28 13:34:38 +0600

    add redux i18n

--- src/index.js
+++ src/index.js
@@ -2,10 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { BrowserRouter } from 'react-router-dom';
+import { Trans } from "react-i18next";
+import './i18n'
+import { RingLoader } from 'react-spinners';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  //<React.StrictMode>
+  <React.Suspense fallback={<RingLoader color="#36d7b7"
+    cssOverride={{ margin: "280px auto" }}
+    loading
+    size={200} />}>
+    <Trans i18nKey=''>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Trans>
+  </React.Suspense>
+  //</React.StrictMode >
 );
