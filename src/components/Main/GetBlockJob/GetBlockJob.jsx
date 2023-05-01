a7805fed531593c860c88bd44e0b01afef8ce1ae
Ammarz83~ <sapitovammar@gmail.com>
2023-05-01 16:46:28 +0600

    commit feat/GetJobBlock

--- src/components/Main/GetBlockJob/GetBlockJob.jsx
+++ src/components/Main/GetBlockJob/GetBlockJob.jsx
@@ -5,7 +5,12 @@ function GetBlockJob() {
   return (
     <div className={css.wrapper}>
         <div className='container'>
-
+            <h1>Get The Right Job For You</h1>
+            <p>Subscribe to get updated on latest and relevant career opportunities</p>
+            <div className={css.inp}>
+                <input placeholder='Enter your email' type="text"/>
+                <button>Subscribe</button>
+            </div>
         </div>
     </div>
   )
