296f880e6d29569c60efd76f123a57b0fd75b5f0
Ammarz83~ <sapitovammar@gmail.com>
2023-05-01 15:45:57 +0600

    commit feat/DownloadAppBlock

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import "./App.scss";
-import DownloadAppBlock from "./components/Main/DownloadAppBlock/DownloadAppBlock";
-import GetBlockJob from "./components/Main/GetBlockJob/GetBlockJob";
+
 import HomePage from "./pages/HomePage";
 
 function App() {
   return (
     <div >
         <HomePage/>
-        {/* <DownloadAppBlock /> */}
-        {/* <GetBlockJob /> */}
       </div>
   );
 }
