d1b943cc145fd257bcda46665b569cd76113e44f
Ammarz83~ <sapitovammar@gmail.com>
2023-05-05 20:38:07 +0600

    commit feat/newDownloadAppN

--- src/components/Main/DownloadAppBlock/DownloadAppBlock.jsx
+++ src/components/Main/DownloadAppBlock/DownloadAppBlock.jsx
@@ -11,27 +11,6 @@ function DownloadAppBlock() {
     return (
         <div className={css.wrapper}>
             <div className='container'>
-                <div className={css.left}>
-                    <h1 className={css.title}>Download now</h1>
-                    <p className={css.text}>Now finding the new job just got even easier with our new app!</p>
-                    <div className={css.two__btns}>
-                        <button>
-                            <img src={apple} alt="" />
-                            <div className={css.btn__info}>
-                                <p>Download on the </p>
-                                <h1>Apple Store</h1>
-                            </div>
-                        </button>
-                        <button>
-                            <img src={googleP} alt="" />
-                            <div className={css.btn__info}>
-                                <p>Download from</p>
-                                <h1>Google Play</h1>
-                            </div>
-                        </button>
-                    </div>
-                </div>
-                <img className={css.main__img} src={bigPhone} alt="" />
                 <div className={css.components}>
                     <div className={css.left}>
                         <div className={css.info}>
