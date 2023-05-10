ef9ecd468b671c89eab1aed2d6f9e7e93eef64e3
kerexwe <adylovakeremet@gmail.com>
2023-05-10 16:02:42 +0600

    feat/fix

--- src/pages/CareerDetails.jsx
+++ src/pages/CareerDetails.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Header from '../components/Header/Header'
 import Hero from '../components/CareerDetails/Hero/Hero'
+import Description from '../components/CareerDetails/Description/Description'
 
 const CareerDetails = () => {
   return (
 	<div>
-	  <Header/>
+	  {/* <Header/> */}
 	  <Hero/>
+	  {/* <Description/> */}
 	</div>
   )
 }
