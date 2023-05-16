a7326c5179c22cbee16a9e4727bc8d4a2fe51658
kerexwe <adylovakeremet@gmail.com>
2023-05-16 19:25:13 +0600

    feat/discription

---
+++ src/components/CareerDetails/Location/Location.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import scss from './Location.module.scss'
+import map from '../../../assets/images/Benefits/map.svg'
+import { useTranslation } from 'react-i18next';
+import { Map, Placemark, YMaps } from 'react-yandex-maps';
+
+
+function Location(props) {
+    const { t } = useTranslation()
+
+    return (
+        <div className={scss.wrap} style={{width : "100%"}}>
+            <h1>{t("location.loc")}</h1>
+            <YMaps>
+                <div className={scss.map} style={{width : "100%"}}>
+                    <Map
+                        width={700}
+                        height={500}
+                        defaultState={{
+                            center: [42.843580, 74.595185],
+                            zoom: 9
+                        }}
+                        
+                    >
+                        <Placemark geometry={[42.843580, 74.595185]}/>
+                    </Map>
+                </div>
+            </YMaps>
+            {/* <img src={map} alt="img" /> */}
+        </div>
+    );
+}
+
+export default Location;
--- src/pages/CareerDetails.jsx
+++ src/pages/CareerDetails.jsx
@@ -1,15 +1,20 @@
 import React from 'react'
 import Header from '../components/Header/Header'
+import Description from '../components/CareerDetails/Description/Description'
 import Footer from '../components/Footer/Footer';
+import Hero from '../components/CareerDetails/Hero/Hero';
 
 const CareerDetails = () => {
-  return (
-	<div>
-	  <Header/>
-	  {/* <Description/> */}
-    <Footer/>
-	</div>
-  )
+	return (
+		<div>
+			<div className='container'>
+			<Header />
+			</div>
+			<Hero />
+			<Description />
+			{/* <Footer /> */}
+		</div>
+	)
 }
 
 export default CareerDetails
