f7310c786c4db5da657ac463da2ac55d01195a30
SBek Ibraimkulov <85980773+Serenbek@users.noreply.github.com>
2023-05-16 20:27:44 +0600

    Revert "feat/description"

--- src/pages/CareerDetails.jsx
+++ src/pages/CareerDetails.jsx
@@ -1,20 +1,15 @@
 import React from 'react'
 import Header from '../components/Header/Header'
-import Description from '../components/CareerDetails/Description/Description'
 import Footer from '../components/Footer/Footer';
-import Hero from '../components/CareerDetails/Hero/Hero';
 
 const CareerDetails = () => {
-	return (
-		<div>
-			<div className='container'>
-			<Header />
-			</div>
-			<Hero />
-			<Description />
-			{/* <Footer /> */}
-		</div>
-	)
+  return (
+	<div>
+	  <Header/>
+	  {/* <Description/> */}
+    <Footer/>
+	</div>
+  )
 }
 
 export default CareerDetails
